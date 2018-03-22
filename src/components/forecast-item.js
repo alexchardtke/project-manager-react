7948c4d16ed1c8057dadc659faa44f9f2c16d753
Alex Hardtke <Alex@Alexs-Mac-mini.local>
2018-03-22 17:23:24 -0500

    added actual temps to forecast-item

--- src/components/forecast-item.js
+++ src/components/forecast-item.js
@@ -5,6 +5,19 @@ class ForecastItem extends Component {
 
   render() {
     // console.log(this.props);
+    let actualLow, actualHigh = null;
+
+    if (!this.props.forecast.actualLow) {
+      actualLow = '-';
+    } else {
+      actualLow = this.props.forecast.actualLow;
+    }
+    
+    if (!this.props.forecast.actualHigh) {
+      actualHigh = '-';
+    } else {
+      actualHigh = this.props.forecast.actualHigh;
+    }
     return (
       <div>
         <div className="row">
@@ -12,10 +25,12 @@ class ForecastItem extends Component {
             <strong>{this.props.forecast.date}</strong>
           </li>
           <li className="ForecastItem col-xs-4">
-            Hi: {this.props.forecast.high}
+            Forecast Hi: {this.props.forecast.high} <br/>
+            Actual Hi: {actualHigh}
           </li>
           <li className="ForecastItem col-xs-4">
-            Low: {this.props.forecast.low}
+            Forecast Low: {this.props.forecast.low} <br/>
+            Actual Low: {actualLow}
           </li>
         </div>
         <hr />
--- src/data.js
+++ src/data.js
@@ -3,452 +3,632 @@ export default {
     {
       date: '2/25',
       high: '32',
-      low: '16'
+      low: '16',
+      actualHigh: '30',
+      actualLow: '16'
     },
     {
       date: '2/26',
       high: '39',
-      low: '22'
+      low: '22',
+      actualHigh: '43',
+      actualLow: '9'
     },
     {
       date: '2/27',
       high: '38',
-      low: '18'
+      low: '18',
+      actualHigh: '44',
+      actualLow: '15'
     },
     {
       date: '2/28',
       high: '40',
-      low: '27'
+      low: '27',
+      actualHigh: '47',
+      actualLow: '16'
     },
     {
       date: '3/1',
       high: '38',
-      low: '18'
+      low: '18',
+      actualHigh: '41',
+      actualLow: '22'
     },
     {
       date: '3/2',
       high: '37',
-      low: '22'
+      low: '22',
+      actualHigh: '40',
+      actualLow: '12'
     },
     {
       date: '3/3',
       high: '37',
-      low: '25'
+      low: '25',
+      actualHigh: '45',
+      actualLow: '33'
     },
     {
       date: '3/4',
       high: '39',
-      low: '24'
+      low: '24',
+      actualHigh: '41',
+      actualLow: '34'
     },
     {
       date: '3/5',
       high: '33',
-      low: '14'
+      low: '14',
+      actualHigh: '38',
+      actualLow: '28'
     },
     {
       date: '3/6',
       high: '28',
-      low: '13'
+      low: '13',
+      actualHigh: '33',
+      actualLow: '23'
     },
     {
       date: '3/7',
       high: '36',
-      low: '14'
+      low: '14',
+      actualHigh: '28',
+      actualLow: '11'
     },
     {
       date: '3/8',
       high: '28',
-      low: '19'
+      low: '19',
+      actualHigh: '29',
+      actualLow: '10'
     },
     {
       date: '3/9',
       high: '33',
-      low: '17'
+      low: '17',
+      actualHigh: '37',
+      actualLow: '14'
     },
     {
       date: '3/10',
       high: '36',
-      low: '19'
+      low: '19',
+      actualHigh: '34',
+      actualLow: '20'
     },
     {
       date: '3/11',
       high: '39',
-      low: '23'
+      low: '23',
+      actualHigh: '36',
+      actualLow: '28'
     },
     {
       date: '3/12',
       high: '34',
-      low: '27'
+      low: '27',
+      actualHigh: '39',
+      actualLow: '20'
     },
     {
       date: '3/13',
       high: '33',
-      low: '24'
+      low: '24',
+      actualHigh: '36',
+      actualLow: '21'
     },
     {
       date: '3/14',
       high: '41',
-      low: '25'
+      low: '25',
+      actualHigh: '45',
+      actualLow: '23'
     },
     {
       date: '3/15',
       high: '47',
-      low: '28'
+      low: '28',
+      actualHigh: '38',
+      actualLow: '26'
     },
     {
       date: '3/16',
       high: '47',
-      low: '30'
+      low: '30',
+      actualHigh: '39',
+      actualLow: '20'
     },
     {
       date: '3/17',
       high: '39',
-      low: '25'
+      low: '25',
+      actualHigh: '49',
+      actualLow: '19'
     },
     {
       date: '3/18',
       high: '48',
-      low: '26'
+      low: '26',
+      actualHigh: '41',
+      actualLow: '23'
     },
     {
       date: '3/19',
       high: '39',
-      low: '23'
+      low: '23',
+      actualHigh: '42',
+      actualLow: '31'
     },
     {
       date: '3/20',
       high: '39',
-      low: '18'
+      low: '18',
+      actualHigh: '31',
+      actualLow: '19'
     },
     {
       date: '3/21',
       high: '36',
-      low: '17'
+      low: '17',
+      actualHigh: '37',
+      actualLow: '19'
     },
     {
       date: '3/22',
       high: '35',
-      low: '17'
+      low: '17',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '3/23',
       high: '36',
-      low: '20'
+      low: '20',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '3/24',
       high: '38',
-      low: '20'
+      low: '20',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '3/25',
       high: '40',
-      low: '32'
+      low: '32',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '3/26',
       high: '52',
-      low: '27'
+      low: '27',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '3/27',
       high: '51',
-      low: '22'
+      low: '22',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '3/28',
       high: '44',
-      low: '20'
+      low: '20',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '3/29',
       high: '42',
-      low: '20'
+      low: '20',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '3/30',
       high: '42',
-      low: '24'
+      low: '24',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '3/31',
       high: '48',
-      low: '22'
+      low: '22',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '4/1',
       high: '47',
-      low: '24'
+      low: '24',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '4/2',
       high: '45',
-      low: '33'
+      low: '33',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '4/3',
       high: '55',
-      low: '39'
+      low: '39',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '4/4',
       high: '57',
-      low: '36'
+      low: '36',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '4/5',
       high: '60',
-      low: '27'
+      low: '27',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '4/6',
       high: '55',
-      low: '29'
+      low: '29',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '4/7',
       high: '47',
-      low: '28'
+      low: '28',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '4/8',
       high: '48',
-      low: '27'
+      low: '27',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '4/9',
       high: '49',
-      low: '26'
+      low: '26',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '4/10',
       high: '48',
-      low: '27'
+      low: '27',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '4/11',
       high: '49',
-      low: '24'
+      low: '24',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '4/12',
       high: '48',
-      low: '26'
+      low: '26',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '4/13',
       high: '49',
-      low: '27'
+      low: '27',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '4/14',
       high: '52',
-      low: '27'
+      low: '27',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '4/15',
       high: '52',
-      low: '28'
+      low: '28',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '4/16',
       high: '53',
-      low: '31'
+      low: '31',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '4/17',
       high: '53',
-      low: '31'
+      low: '31',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '4/18',
       high: '54',
-      low: '29'
+      low: '29',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '4/19',
       high: '53',
-      low: '32'
+      low: '32',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '4/20',
       high: '56',
-      low: '45'
+      low: '45',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '4/21',
       high: '62',
-      low: '49'
+      low: '49',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '4/22',
       high: '67',
-      low: '45'
+      low: '45',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '4/23',
       high: '67',
-      low: '44'
+      low: '44',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '4/24',
       high: '69',
-      low: '34'
+      low: '34',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '4/25',
       high: '59',
-      low: '33'
+      low: '33',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '4/26',
       high: '54',
-      low: '38'
+      low: '38',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '4/27',
       high: '57',
-      low: '39'
+      low: '39',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '4/28',
       high: '65',
-      low: '46'
+      low: '46',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '4/29',
       high: '65',
-      low: '40'
+      low: '40',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '4/30',
       high: '61',
-      low: '35'
+      low: '35',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '5/1',
       high: '58',
-      low: '34'
+      low: '34',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '5/2',
       high: '56',
-      low: '34'
+      low: '34',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '5/3',
       high: '55',
-      low: '33'
+      low: '33',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '5/4',
       high: '58',
-      low: '32'
+      low: '32',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '5/5',
       high: '58',
-      low: '34'
+      low: '34',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '5/6',
       high: '60',
-      low: '37'
+      low: '37',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '5/7',
       high: '61',
-      low: '40'
+      low: '40',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '5/8',
       high: '63',
-      low: '44'
+      low: '44',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '5/9',
       high: '61',
-      low: '39'
+      low: '39',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '5/10',
       high: '63',
-      low: '39'
+      low: '39',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '5/11',
       high: '63',
-      low: '37'
+      low: '37',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '5/12',
       high: '62',
-      low: '39'
+      low: '39',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '5/13',
       high: '64',
-      low: '37'
+      low: '37',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '5/14',
       high: '64',
-      low: '39'
+      low: '39',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '5/15',
       high: '65',
-      low: '41'
+      low: '41',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '5/16',
       high: '66',
-      low: '44'
+      low: '44',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '5/17',
       high: '69',
-      low: '46'
+      low: '46',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '5/18',
       high: '73',
-      low: '54'
+      low: '54',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '5/19',
       high: '75',
-      low: '42'
+      low: '42',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '5/20',
       high: '70',
-      low: '41'
+      low: '41',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '5/21',
       high: '69',
-      low: '53'
+      low: '53',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '5/22',
       high: '75',
-      low: '56'
+      low: '56',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '5/23',
       high: '80',
-      low: '56'
+      low: '56',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '5/24',
       high: '83',
-      low: '43'
+      low: '43',
+      actualHigh: '',
+      actualLow: ''
     },
     {
       date: '5/25',
       high: '79',
-      low: '40'
+      low: '40',
+      actualHigh: '',
+      actualLow: ''
     },
 
   ]
